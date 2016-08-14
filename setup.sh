a852fbf9312e19ec82ce628217312c31631f4d2b
Pavel Kuropatkin <pasha.rgmu@gmail.com>
2016-08-14 11:01:40 -0500

    working on the install script

--- setup.sh
+++ setup.sh
@@ -1,3 +1,20 @@
 #!/usr/bin/env bash
 
+# Install dependencies first
+brew install macvim --with-override-system-vim
+brew install cmake
+
+# copy required files
+cp setup/vimrc.txt ~/.vimrc
+rm -rf ~/.vim
+cp -r setup/dot_vim ~/.vim
+cp setup/dot_tern_config ~/.tern_config
+cp setup/ycm_extra_conf.py ~/.ycm_extra_conf.py
+
+# setup vim plugin manager
+
+# setup autocomplete plugin
+cd ~/.vim/bundle/YouCompleteMe
+./install.py --clang-completer --tern-completer
+
 
