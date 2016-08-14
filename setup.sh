f80a1aa6bbb63e8dac151e844a38400ced545141
Pavel Kuropatkin <pasha.rgmu@gmail.com>
2016-08-14 11:13:29 -0500

    mac script has been added

--- setup.sh
+++ setup.sh
@@ -12,9 +12,12 @@ cp setup/dot_tern_config ~/.tern_config
 cp setup/ycm_extra_conf.py ~/.ycm_extra_conf.py
 
 # setup vim plugin manager
+git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
+vim +PluginInstall +qall
 
 # setup autocomplete plugin
 cd ~/.vim/bundle/YouCompleteMe
 ./install.py --clang-completer --tern-completer
 
-
+# everything is done
+echo 'Your vim setup is finished. Happy hacking!'
